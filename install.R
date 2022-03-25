153fde29754339a48ffdb2a56b312e997ad2d17e
PderyckeSciensano <98525592+PderyckeSciensano@users.noreply.github.com>
2022-03-25 15:10:42 +0100

    Update install.R

--- install.R
+++ install.R
@@ -11,3 +11,14 @@ install.packages('dplyr')
 install.packages('lubridate')
 require(devtools)
 install_version("rmarkdown", version = "2.13", repos = "https://cran.r-project.org")
+update.packages(ask = FALSE, checkBuilt = TRUE)
+packages = c("tidyverse","readr","ggplot2","dplyr","lubridate","plotly","DT","janitor")
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
