aca895496bbbf0168b4b2935a1cc8d778bcb0de4
PderyckeSciensano <98525592+PderyckeSciensano@users.noreply.github.com>
2022-11-21 17:07:20 +0100

    Update install.R

--- install.R
+++ install.R
@@ -11,7 +11,7 @@ require(devtools)
 install_version("rmarkdown", version = "2.13", repos = "https://cran.r-project.org")
 install_version("tidyverse", version = "1.3.1", repos = "https://cran.r-project.org")
 update.packages(ask = FALSE, checkBuilt = TRUE)
-packages = c("tidyverse","readr","ggplot2","dplyr","lubridate","plotly","DT","janitor")
+packages = c("tidyverse","readr","ggplot2","dplyr","lubridate","plotly","DT","janitor","htmlTable","kableExtra","questionr","wrapr","zoo")
 package.check <- lapply(
   packages,
   FUN = function(x) {
